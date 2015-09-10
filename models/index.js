08398f78995abcc5ff785726bdff41f85a889b94
Nathan Ockerman <ockerman.ns@gmail.com>
2015-09-10 13:10:16 -0700

    3rd attempt at Heroku.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/write_start_app");
+/*mongoose.connect("mongodb://localhost/write_start_app");*/
 
 mongoose.connect( process.env.MONGOLAB_URI ||
                     process.env.MONGOHQ_URL || 
-                    "YOUR OWN LOCAL URL HERE" )
+                    "https://write-start.herokuapp.com" )
 
 module.exports.Submission = require("./user.js")
 module.exports.User = require("./user.js");
